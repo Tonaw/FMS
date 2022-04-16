b8ad8c9d2d3f098dbc7a46ea9b85505e0e07cd78
Adedolapo Adetona <adedolapolawadetona@gmail.com>
2022-04-16 19:25:44 +0100

    Controller Setup

--- FMS.Data/Models/User.cs
+++ FMS.Data/Models/User.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-namespace FMS.Data.Models {
-    
+namespace FMS.Data.Models 
+{    
     public enum Role { admin, manager, guest }
 
-    public class User {
+    public class User 
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
--- FMS.Web/Controllers/VehicleController.cs
+++ FMS.Web/Controllers/VehicleController.cs
@@ -7,8 +7,47 @@ using FMS.Web.Models;
 
 namespace FMS.Web.Controllers;
 
-public class VehicleController : Controller
+public class VehicleController : BaseController
 {
     // provide suitable controller actions
+
+    private IFleetService svc;
+
+    public VehicleController()
+    {
+        svc = new FleetServiceDb();
+    }
+
+
+    //GET/ Vehicle
+
+    public IActionResult Index()
+    {
+        var vehicles = svc.GetVehicles();
+
+        return View(vehicles);
+    }
+
+    //GET //Vehicles/details/{vehicle id}
+
+    public IActionResult Details(int id)
+    {
+        var s = svc.GetVehicle(id);
+
+        if (s == null)
+        {
+            Alert($"Vehicle {id} not found", AlertType.warning);
+            return RedirectToAction(nameof(Index));
+        }
+
+        //pass vehicle to view
+        return View(s);
+    }
+
+
+    
+
+
+
 
 }
